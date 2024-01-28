c9a1f1a44f3c7c8b4da033320cd6392bf4e3c3fd
vadim <vadim.urazov@gmail.com>
2024-01-28 13:03:29 +0600

    Change classes

--- buscompany/src/main/java/net/thumbtack/buscompany/validate/Phone.java
+++ buscompany/src/main/java/net/thumbtack/buscompany/validate/Phone.java
@@ -1,18 +1,23 @@
 package net.thumbtack.buscompany.validate;
 
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import java.lang.annotation.*;
 
 @Documented
 @Constraint(validatedBy = PhoneValidator.class)
 @Target({ElementType.METHOD, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Phone {
-    String message() default "Error phone!";
 
-    Class<?>[] groups() default {};
+  String message() default "Error phone!";
 
-    Class<? extends Payload>[] payload() default {};
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
 }
 
--- buscompany/src/test/java/net/thumbtack/buscompany/config/RestClientConfig.java
+++ buscompany/src/test/java/net/thumbtack/buscompany/config/RestClientConfig.java
@@ -1,28 +1,27 @@
 package net.thumbtack.buscompany.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.util.CollectionUtils;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Configuration
 public class RestClientConfig {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
+  @Bean
+  public RestTemplate restTemplate() {
+    RestTemplate restTemplate = new RestTemplate();
 
-        List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
-        if (CollectionUtils.isEmpty(interceptors)) {
-            interceptors = new ArrayList<>();
-        }
-
-        restTemplate.setInterceptors(interceptors);
-        return restTemplate;
+    List<ClientHttpRequestInterceptor> interceptors = restTemplate.getInterceptors();
+    if (CollectionUtils.isEmpty(interceptors)) {
+      interceptors = new ArrayList<>();
     }
+
+    restTemplate.setInterceptors(interceptors);
+    return restTemplate;
+  }
 
 }
